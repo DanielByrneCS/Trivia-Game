8fb5a6f7ed8b442896d66abe5826e04de82c3c80
Dan Byrne <g00430946@atu.ie>
2024-12-05 23:37:39 +0000

    Takes in Json correctly now

--- MauiApp1/Game.xaml.cs
+++ MauiApp1/Game.xaml.cs
@@ -1,6 +1,11 @@
+using System.IO;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MauiApp1;
 
@@ -21,29 +26,100 @@ public partial class Game : ContentPage
     };
 
     private HttpClient httpClient;
+    QuestionResponse questionResponse;
 
-
-
-
+    public string Difficulty {  get; set; }
+    public string NumberOfQuestions {  get; set; }
+    public string GameType { get; set; }
     public Game(int difficulty, int numQuestions, int type)
     {
         InitializeComponent();
-        difficult.Text = difficulty.ToString();
-        questions.Text = numQuestions.ToString();
-        typea.Text = type.ToString();
         httpClient = new HttpClient();
-        GameLoop(GetQuestions(difficulty, numQuestions, type));
-
+        GetQuestions(difficulty, numQuestions, type );
+        GameLoop(difficulty, numQuestions, type);
+        QuestionsLabel(numQuestions);
+        DifficultyLabel(difficulty);
+        TypeLabel(type);
+        BindingContext = this;''
 
     }
 
-    private async void GameLoop(Task<string> questions)
+    private void DifficultyLabel(int difficulty)
     {
-       // turns the Task<string> to a string to deserialise the json into question class
-       string strQuestions = await questions;
-       var deserializedQuestions = JsonSerializer.Deserialize<List<Question>>(strQuestions);
+        switch (difficulty)
+        {
+            case 0:
+                Difficulty = "Easy";
+                break;
+            case 1:
+                Difficulty = "Medium";
+                break;
+            case 2:
+                Difficulty = "Hard";
+                break;
+            default:
+                Difficulty = "You shouldn't see this ever";
+                break;
+        }
+    }
 
-  
+    private void TypeLabel(int type)
+    {
+        switch (type)
+        {
+            case 0:
+                GameType = "Multiple Choice";
+                break;
+            case 1:
+                GameType = "True/False";
+                break;
+            default:
+                GameType = "This shouldn't happen";
+                break;
+        }
+    }
+
+    void QuestionsLabel(int numq)
+    {
+        switch (numq)
+        {
+            case 5:
+                NumberOfQuestions = "5 Questions";
+                break;
+            case 10:
+                NumberOfQuestions = "10 Questions";
+                break;
+            case 15:
+                NumberOfQuestions = "15 Questions";
+                break;
+            case 20:
+                NumberOfQuestions = "20 Questions";
+                break;
+
+            default:
+                NumberOfQuestions = "This shouldn't happen";
+                break;
+        }
+    }
+    private async void DisplayQuestion()
+    {
+        await Task.Delay(1000);
+        foreach (Question question in questionResponse.results)
+        {
+            questionTitle.Text += question.question + "\n";
+        }
+        
+    }
+
+    private async void GameLoop(int difficulty, int numQuestions, int type)
+    {
+
+        for(int i = 0; i < numQuestions; i++)
+        {
+
+        }
+       
+
     }
 
     string APILinkCreator(int difficulty, int numQuestions, int questionType)
@@ -56,13 +132,26 @@ public partial class Game : ContentPage
 
     }
 
-    public async Task<string> GetQuestions(int difficulty, int numQuestions, int questionType)
+    public async Task GetQuestions(int difficulty, int numQuestions, int questionType)
     {
-        var jsonQuestions = await httpClient.GetAsync(APILinkCreator(difficulty, numQuestions, questionType));
-        if (jsonQuestions.IsSuccessStatusCode)
+        try
         {
-            return await jsonQuestions.Content.ReadAsStringAsync();
+    
+            var response = await httpClient.GetAsync("https://opentdb.com/api.php?amount=10&difficulty=medium");
+            if (response.IsSuccessStatusCode)
+            {
+                string contents = await response.Content.ReadAsStringAsync();
+                // Below line from Json2CSharp 
+                questionResponse = JsonSerializer.Deserialize<QuestionResponse>(contents);
+                
+            }
         }
-        return "";
+        catch (Exception ex)
+        {
+            await Shell.Current.DisplayAlert("Error in loading Questions", ex.Message, "OK");
+        }
+    
     }
+
+
 }
--- MauiApp1/Question.cs
+++ MauiApp1/Question.cs
@@ -6,14 +6,19 @@ using System.Threading.Tasks;
 
 namespace MauiApp1
 {
-    class Question
+    public class Question
     {
+        public string category { get; set; }
         public string type { get; set; }
         public string difficulty { get; set; }
-        public string category { get; set; }
         public string question { get; set; }
         public string correct_answer { get; set; }
         public List<string> incorrect_answers { get; set; }
+    }
+
+    public class QuestionResponse
+    {
+        public List<Question> results { get; set; }
     }
 
 
